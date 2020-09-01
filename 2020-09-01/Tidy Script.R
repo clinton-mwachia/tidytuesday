35dea5d0ea6f04dff6179368c7920d86245f6afb
Clinton Moshe <clintonmwachia9@gmail.com>
2020-09-01 14:57:33 +0300

    change variable names

--- 2020-09-01/Tidy Script.R
+++ 2020-09-01/Tidy Script.R
@@ -8,18 +8,18 @@ library(tidyverse)
 
 data <- tt_load("2020-09-01")
 
-arable_land_pin <- data$arable_land_pin
-cereal_crop_yield_vs_fertilizer_application <- data$cereal_crop_yield_vs_fertilizer_application
-cereal_yields_vs_tractor_inputs_in_agriculture <- data$cereal_yields_vs_tractor_inputs_in_agriculture
+arable_land <- data$arable_land_pin
+fertilizer <- data$cereal_crop_yield_vs_fertilizer_application
+tractor <- data$cereal_yields_vs_tractor_inputs_in_agriculture
 key_crop_yields <- data$key_crop_yields
-land_use_vs_yield_change_in_cereal_production <- data$land_use_vs_yield_change_in_cereal_production
+land_use <- data$land_use_vs_yield_change_in_cereal_production
 
 # data structure
-glimpse(arable_land_pin)
-glimpse(cereal_crop_yield_vs_fertilizer_application)
-glimpse(cereal_yields_vs_tractor_inputs_in_agriculture)
+glimpse(arable_land)
+glimpse(fertilizer)
+glimpse(tractor)
 glimpse(key_crop_yields)
-glimpse(land_use_vs_yield_change_in_cereal_production)
+glimpse(land_use)
 
 
 # na values
@@ -27,35 +27,36 @@ glimpse(land_use_vs_yield_change_in_cereal_production)
 col_na_percentage <- function(data){
   (colSums(is.na(data))/ nrow(data))
 }
-col_na_percentage(arable_land_pin)
-col_na_percentage(cereal_crop_yield_vs_fertilizer_application)
-col_na_percentage(cereal_yields_vs_tractor_inputs_in_agriculture)
+col_na_percentage(arable_land)
+col_na_percentage(fertilizer)
+col_na_percentage(tractor)
 col_na_percentage(key_crop_yields)
-col_na_percentage(land_use_vs_yield_change_in_cereal_production)
+col_na_percentage(land_use)
 
 
 # joining the data
-arable_comb_cereal_crop_yield_vs_fetilizer <- arable_land_pin %>%
-  inner_join(cereal_crop_yield_vs_fertilizer_application)
+arable_land_comb_fetilizer <- arable_land %>%
+  inner_join(fertilizer)
 
-cereal_yields_vs_tractor_inputs_in_agriculture$Year <- as.double(cereal_yields_vs_tractor_inputs_in_agriculture$Year)
+tractor$Year <- as.double(tractor$Year)
 # one
-arable_comb_cereal_crop_yield_vs_fetilizer_comb_cereals_yield_vs_tractor_inputs <- arable_comb_cereal_crop_yield_vs_fetilizer %>%
-  inner_join(cereal_yields_vs_tractor_inputs_in_agriculture)
+arable_land_comb_fetilizer_comb_tractor <- arable_land_comb_fetilizer %>%
+  inner_join(tractor)
   
 # two
-one_comb_cereal_yields_tractor <- arable_comb_cereal_crop_yield_vs_fetilizer_comb_cereals_yield_vs_tractor_inputs%>%
-  inner_join(cereal_yields_vs_tractor_inputs_in_agriculture)
-
-# three
-two_comb_key_crop_yields <- one_comb_cereal_yields_tractor %>%
+key_crop_yields_tractor_vs_one <- arable_land_comb_fetilizer %>%
   inner_join(key_crop_yields)
 
-land_use_vs_yield_change_in_cereal_production$Year <- as.double(land_use_vs_yield_change_in_cereal_production$Year)
+# three
+two_comb_key_crop_yields <- key_crop_yields_tractor_vs_one %>%
+  inner_join(key_crop_yields)
+
+land_use$Year <- as.double(land_use$Year)
 three_comb_land_use_vs_yield_change_in_cereal <- two_comb_key_crop_yields %>%
-  inner_join(land_use_vs_yield_change_in_cereal_production)
+  inner_join(land_use)
 
 
 data <- three_comb_land_use_vs_yield_change_in_cereal
 
 col_na_percentage(data)
+colnames(data)
