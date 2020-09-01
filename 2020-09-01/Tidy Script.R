249e43ede8db1e48f822d1d5d2f7efe80daa6958
Clinton Moshe <clintonmwachia9@gmail.com>
2020-09-01 14:28:23 +0300

    inner join all the data

--- 2020-09-01/Tidy Script.R
+++ 2020-09-01/Tidy Script.R
@@ -23,18 +23,39 @@ glimpse(land_use_vs_yield_change_in_cereal_production)
 
 
 # na values
-# percentage of na per colum
-(colSums(is.na(arable_land_pin)) / nrow(arable_land_pin)) * 100
-(colSums(is.na(cereal_crop_yield_vs_fertilizer_application)) / nrow(cereal_yields_vs_tractor_inputs_in_agriculture)) * 100
-(colSums(is.na(cereal_yields_vs_tractor_inputs_in_agriculture)) / nrow(cereal_yields_vs_tractor_inputs_in_agriculture)) * 100
-(colSums(is.na(key_crop_yields)) / nrow(key_crop_yields)) * 100
-(colSums(is.na(land_use_vs_yield_change_in_cereal_production)) / nrow(land_use_vs_yield_change_in_cereal_production)) * 100
+# percentage of na per column
+col_na_percentage <- function(data){
+  (colSums(is.na(data))/ nrow(data))
+}
+col_na_percentage(arable_land_pin)
+col_na_percentage(cereal_crop_yield_vs_fertilizer_application)
+col_na_percentage(cereal_yields_vs_tractor_inputs_in_agriculture)
+col_na_percentage(key_crop_yields)
+col_na_percentage(land_use_vs_yield_change_in_cereal_production)
+
 
 # joining the data
 arable_comb_cereal_crop_yield_vs_fetilizer <- arable_land_pin %>%
   inner_join(cereal_crop_yield_vs_fertilizer_application)
 
-arabl
+cereal_yields_vs_tractor_inputs_in_agriculture$Year <- as.double(cereal_yields_vs_tractor_inputs_in_agriculture$Year)
+# one
 arable_comb_cereal_crop_yield_vs_fetilizer_comb_cereals_yield_vs_tractor_inputs <- arable_comb_cereal_crop_yield_vs_fetilizer %>%
   inner_join(cereal_yields_vs_tractor_inputs_in_agriculture)
   
+# two
+one_comb_cereal_yields_tractor <- arable_comb_cereal_crop_yield_vs_fetilizer_comb_cereals_yield_vs_tractor_inputs%>%
+  inner_join(cereal_yields_vs_tractor_inputs_in_agriculture)
+
+# three
+two_comb_key_crop_yields <- one_comb_cereal_yields_tractor %>%
+  inner_join(key_crop_yields)
+
+land_use_vs_yield_change_in_cereal_production$Year <- as.double(land_use_vs_yield_change_in_cereal_production$Year)
+three_comb_land_use_vs_yield_change_in_cereal <- two_comb_key_crop_yields %>%
+  inner_join(land_use_vs_yield_change_in_cereal_production)
+
+
+data <- three_comb_land_use_vs_yield_change_in_cereal
+
+col_na_percentage(data)
