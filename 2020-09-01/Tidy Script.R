dfe907f5dd5ca7c2031d9d5c9c7e71837263c5e5
Clinton Moshe <clintonmwachia9@gmail.com>
2020-09-02 14:53:09 +0300

    dealing with na values

--- 2020-09-01/Tidy Script.R
+++ 2020-09-01/Tidy Script.R
@@ -1,10 +1,11 @@
-#install.packages("tidytuesdayR")
-#setwd('F:\\R\\Mycodes\\2020\\tidytuesday\\2020-09-01')
+# install.packages("tidytuesdayR")
+# setwd('F:\\R\\Mycodes\\2020\\tidytuesday\\2020-09-01')
 # getwd()
 
 # loading the package and the data
 library(tidytuesdayR)
 library(tidyverse)
+library(DataExplorer)
 
 data <- tt_load("2020-09-01")
 
@@ -59,4 +60,32 @@ three_comb_land_use_vs_yield_change_in_cereal <- two_comb_key_crop_yields %>%
 data <- three_comb_land_use_vs_yield_change_in_cereal
 
 col_na_percentage(data)
-colnames(data)
+colnames(data1)
+
+# rename the column names
+data1 <- data %>%
+  rename(
+    `Arable land` = `Arable land needed to produce a fixed quantity of crops ((1.0 = 1961))`,
+    `Nitrogen fertilizer (kg per hec)` = `Nitrogen fertilizer use (kilograms per hectare)`,
+    `Cereal yield (ton per hec)` = `Cereal yield (tonnes per hectare)`,
+    `Wheat (ton per hec` = `Wheat (tonnes per hectare)`,
+    `Rice (ton per hec)` = `Rice (tonnes per hectare)`,
+    `Maize (ton per hec` = `Maize (tonnes per hectare)`,
+    `Soybeans (ton per hec` = `Soybeans (tonnes per hectare)`,
+    `Beans (ton per hec` = `Beans (tonnes per hectare)`,
+    `Peas (ton per hec)` = `Peas (tonnes per hectare)`,
+    `Cassava (ton per hec)` = `Cassava (tonnes per hectare)`,
+    `Barley (ton per hec)` = `Barley (tonnes per hectare)`,
+    `Cocoa beans (ton per hec)` = `Cocoa beans (tonnes per hectare)`,
+    `Bananas (ton per hec)` = `Bananas (tonnes per hectare)`,
+    `land change due to cereal production since 1961` = `Change to land area used for cereal production since 1961`
+  )
+
+# plot missing
+profile_missing(data1)
+plot_missing(
+  data1,
+  title = 'Missing values by percentage',
+  missing_only = TRUE
+) +
+  theme(legend.position = 'none') 
