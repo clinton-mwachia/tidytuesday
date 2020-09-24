a154545e089aa46d2faa08536c6619fb59509b52
Clinton Moshe <clintonmwachia9@gmail.com>
2020-09-25 00:28:14 +0300

    Himalayan Climbing Expeditions

--- 2020-09-22/Tidy Script.R
+++ 2020-09-22/Tidy Script.R
@@ -1,5 +1,6 @@
 library(tidytuesdayR)
 library(tidyverse)
+library(DataExplorer)
 
 data <- tt_load("2020-09-22")
 
@@ -25,3 +26,32 @@ str(expeditions)
 expeditions1 <- expeditions %>%
   mutate_if(is.character, as.factor)
 str(expeditions1)
+
+
+#### DEALING WITH NA
+plot_missing(peaks1,title = 'Peaks data')
+plot_missing(members1,title = 'Members data')
+plot_missing(expeditions1,title = 'Expeditions data')
+
+## DROPPING COLUMNS WITH EXTREME NA VAL
+peaks1 <- peaks1 %>%
+  select(-peak_alternative_name)
+
+members1 <- members1 %>%
+  select(-c(injury_type,death_cause,death_height_metres,injury_height_metres))
+
+
+### DATA VIZ FOR PEAS
+# climbing status vs the height
+# to identify how height affects the climbing status
+ggplot(data = peaks1, aes(x=climbing_status,y=height_metres)) +
+  geom_boxplot(outlier.color = 'red') +
+  geom_jitter(width = 0.2)
+hist(peaks1$height_metres)
+
+ggplot(data = peaks1, aes(x=climbing_status,y=height_metres)) +
+  geom_violin() +
+  geom_jitter(width = 0.2) +
+  geom_text()
+
+
