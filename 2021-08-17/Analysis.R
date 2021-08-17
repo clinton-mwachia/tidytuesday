77749d83e0e4761fb84ade166dd1be3d410b35ff
Clinton Moshe <clintonmwachia9@gmail.com>
2021-08-17 11:22:37 +0300

    loaded the data

---
+++ 2021-08-17/Analysis.R
@@ -0,0 +1,10 @@
+# loading the tidy tuesday library
+library(tidytuesdayR)
+
+#tuesdata <- tt_load('2021-08-17')
+tuesdata <- tt_load(2021, week = 34)
+
+# the computer data
+computer <- tuesdata$computer
+
+
