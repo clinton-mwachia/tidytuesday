bae5d4b1cdfd16ed176950656811136a8ffe4544
clinton <clintonmwachia9@gmail.com>
2022-02-04 14:25:34 +0300

    plot dog breed with ranking

--- 2022-02-04/script.R
+++ 2022-02-04/script.R
@@ -1,8 +1,28 @@
+# efficient R
+# testing common R operations
+
 # loading the libraries
 library(tidyverse)
 library(tidytuesdayR)
+library(imager)
 
 # loading the data
-breed_traits <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv')
-trait_description <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/trait_description.csv')
-breed_rank_all <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv')
+bt_url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_traits.csv'
+td_url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/trait_description.csv'
+bra_url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-01/breed_rank.csv'
+
+breed_traits <- read_csv(bt_url)
+trait_description <- read_csv(td_url)
+breed_rank_all <- read_csv(bra_url)
+
+# checking NA values
+colSums(is.na(breed_traits))
+
+colSums(is.na(breed_rank_all))
+
+# plotting the dogs
+par(mfrow=c(1,1))
+for (i in 1:5) {
+  load.image(breed_rank_all$Image[i]) %>%
+    plot(main=paste('Breed: ',breed_rank_all$Breed[i],', 2020 Rank', breed_rank_all$`2020 Rank`[i]))
+}
